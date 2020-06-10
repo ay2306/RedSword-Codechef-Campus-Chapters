86d4cb24b15c2bac3a77269ebba4cb6df5e97889
ay2306 <mahajan.ayush2306@gmail.com>
2020-06-10 19:40:51 +0530

    Initial Commit

---
+++ ay2306/helloWorld.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr {72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100}; 
+    for(int i: arr)cout << (char)i ;
+    return 0;
+}
