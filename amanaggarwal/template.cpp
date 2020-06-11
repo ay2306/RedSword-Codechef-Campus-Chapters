ac097c8bfea7c768204caf04d536d296390422c8
Aman Aggarwal <amanaggarwal014@gmail.com>
2020-06-11 14:02:24 +0530

    Creating my codeforces folder
    Added my template

---
+++ amanaggarwal/template.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pci pair<char, int>
+#define pcc pair<char, char>
+
+#define vi vector <int>
+#define vll vector<long long>
+#define vld vector<long double>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define vvld vector<vld>
+#define vvb vector<vb>
+#define vvc vector<vc>
+#define vvs vector<vs>
+
+#define vpii vector<pii>
+#define vpci vector<pci>
+#define vpcc vector<pcc>
+
+#define all(v) (v).begin(), (v).end()
+#define allrev(v) (v).rbegin(), (v).rend()
+#define allcomp(v) (v).begin(), (v).end(), comp
+#define allrevcomp(v) (v).rbegin(), (v).rend(), comp
+
+const int MOD = 1e9 + 7;
+const ld pi = 3.1415926535897932384626433832;
+
+#define line cout<<"\n";
+#define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+
+void solve(int test_case){
+    cout << "Hello world";
+}
+
+int main(){
+    fast
+    int test = 1;
+    //cin>>test;
+    for(int i = 1; i <= test; i++){
+        solve(i);
+    }
+    return 0;
+}
