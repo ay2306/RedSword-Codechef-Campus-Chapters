f17c18d7868e0afe79e2485fd8baa0622067cdee
harsharora99 <42893467+harsharora99@users.noreply.github.com>
2020-06-15 19:51:00 +0530

    Create task.cpp

---
+++ hdsingharora99/task.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void solve(int tc){
+    
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t=1;
+    //cin>>t;
+    for(int i=1;i<=t;i++){
+        solve(i);
+        cout<<"\n";
+    }
+    
+    return 0;
+}
